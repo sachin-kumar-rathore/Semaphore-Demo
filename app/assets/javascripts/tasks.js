73e2777a45de2536fc5bef910f519ac1e916532f
emu <himtiaz@dohatec.com.bd>
2017-10-31 10:51:10 +0600

    reload tasks after CRUD in a function

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -30,3 +30,17 @@ $(document).on("click", ".nav-pills .nav-item", function () {
 $('.pagination').find('a').each(function() {
   $(this).attr("data-turbolinks", false);
 });
+
+function reloadTasks() {
+  $("#taskFormCenter").modal("hide");
+  var selected_tab_id = $('.nav-item .nav-link.active').closest("li").attr("id");
+
+  if(selected_tab_id == "all_tasks_filter"){
+    $.ajax({
+        url: '/tasks/filter_tasks',
+        type: "GET",
+        data: {filter_id: selected_tab_id}
+    })
+  }
+  else $("#" + selected_tab_id).click();
+}

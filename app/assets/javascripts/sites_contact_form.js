f9dab1d3d835d9e34e69c8b203741a1931e2c8ee
sirajus Salayhin <salayhin@gmail.com>
2017-11-02 00:05:25 +0600

    Ajaxify Update and delete methods for Sites module

--- app/assets/javascripts/sites_contact_form.js
+++ app/assets/javascripts/sites_contact_form.js
@@ -1,13 +1,13 @@
 $('#tokenContactName').tokenInput('/sites/find_contact.json', {
-  crossDomain: false,
-  hintText: 'Contact search',
-  minChars: 2,
-  tokenLimit: 1,
-  resultsFormatter: function(item) {
-    return "<li>" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.name + "</div><div class='email'>" + item.email + "</div></div></li>"
-  },
-  tokenFormatter: function(item) {
-    return "<li><p>Name: " + item.name + "</p><br><p>Email: " + item.email + "</p><br><p>Phone: " + item.cell_phone + "</p></li>"
-  },
-  prePopulate: $('#tokenContactName').data('pre')
+    crossDomain: false,
+    hintText: 'Contact search',
+    minChars: 2,
+    tokenLimit: 1,
+    resultsFormatter: function (item) {
+        return "<li>" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.name + "</div><div class='email'>" + item.email + "</div></div></li>"
+    },
+    tokenFormatter: function (item) {
+        return "<li><p>Name: " + item.name + "</p><br><p>Email: " + item.email + "</p><br><p>Phone: " + item.cell_phone + "</p></li>"
+    },
+    prePopulate: $('#tokenContactName').data('pre')
 });

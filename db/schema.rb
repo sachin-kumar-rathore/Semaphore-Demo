fa82b135dd5db89df0f40e5e2a6c832bd8d44f6a
Sachin <skrathore.ror@gmail.com>
2017-11-03 19:45:53 +0530

    Changes in Dedicated Tasks Section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102072009) do
+ActiveRecord::Schema.define(version: 20171103081117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,7 +168,6 @@ ActiveRecord::Schema.define(version: 20171102072009) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "project_id"
     t.integer "assignee_id"
     t.string "name"
     t.text "description"
@@ -179,6 +178,8 @@ ActiveRecord::Schema.define(version: 20171102072009) do
     t.float "progress", default: 0.0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -215,5 +216,6 @@ ActiveRecord::Schema.define(version: 20171102072009) do
   add_foreign_key "project_contacts", "projects"
   add_foreign_key "projects", "companies"
   add_foreign_key "projects", "organizations"
+  add_foreign_key "tasks", "projects"
   add_foreign_key "users", "organizations"
 end

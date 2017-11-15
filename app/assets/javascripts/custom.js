89de3d1786dbe5811cefbb614d99bc8f39dc4b23
Sachin <skrathore.ror@gmail.com>
2017-11-15 19:48:13 +0530

    Solved token input issue on sites/buildings

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -168,3 +168,20 @@ function reloadSites(id){
     type: "GET"
   });
 }
+
+function tokenInputforContact() {
+  $('#tokenContactName').tokenInput('/sites/find_contact.json', {
+    crossDomain: false,
+    hintText: 'Contact search',
+    minChars: 2,
+    tokenLimit: 1,
+    resultsFormatter: function (item) {
+        return "<li>" + "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>" + item.name + "</div><div class='email'>" + item.email + "</div></div></li>"
+    },
+    tokenFormatter: function (item) {
+        return "<li><p>Name: " + item.name + "</p><br><p>Email: " + item.email + "</p><br><p>Phone: " + item.cell_phone + "</p></li>"
+    },
+    prePopulate: $('#tokenContactName').data('pre')
+  });
+};
+

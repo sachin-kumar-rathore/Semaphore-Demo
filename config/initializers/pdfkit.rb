3237c1689f51e6a3bc5e43617e8537e0481a86b5
Sachin <skrathore.ror@gmail.com>
2018-01-08 20:06:49 +0530

    Merged reporting PR and tweaked sidebar

--- config/initializers/pdfkit.rb
+++
@@ -1,3 +0,0 @@
-PDFKit.configure do |config|
-  config.wkhtmltopdf = Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-end
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,13 +9,19 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  exe_path: '/usr/bin/wkhtmltopdf'
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+#   # Path to the wkhtmltopdf executable: This usually isn't needed if using
+#   # one of the wkhtmltopdf-binary family of gems.
+#   if  Rails.env.production?
+#     WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s}
+#   else
+#     WickedPdf.config = { :exe_path => '/usr/bin/wkhtmltopdf'}
+#   end
+#   #   or
+  exe_path: "#{Rails.root}/bin/wkhtmltopdf"
 
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
+#   # Layout file to be used for all PDFs
+#   # (but can be overridden in `render :pdf` calls)
+#   # layout: 'pdf.html',
 }
+
+

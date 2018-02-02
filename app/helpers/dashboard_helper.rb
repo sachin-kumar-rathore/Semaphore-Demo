3595ea010dbdfad32a3056f844f99a02f4e650bb
imtiaz-emu <imtiaz.emu.it@gmail.com>
2018-02-02 17:48:57 +0600

    Dashboard Charts added, issue: PieChart

---
+++ app/helpers/dashboard_helper.rb
@@ -0,0 +1,58 @@
+module DashboardHelper
+
+  require 'json'
+
+  def get_project_types(results)
+    filtered_project_types = results['project_type_id'].keys
+        .inject({}) {|sum, n| sum.merge(results['project_type_id'][n]) {|key, v1, v2| v1+v2} }
+        .sort_by {|key, val| val.count}.reverse.first(4)
+
+    project_types_to_show = filtered_project_types.collect{|type| type[0]}
+    seriesList = []
+    labels = current_org.project_types.filter_by_id(project_types_to_show).pluck(:name)
+    results['project_type_id'].keys.each do |year|
+      series = []
+      project_types_to_show.each do |type_id|
+        series.push(results['project_type_id'][year][type_id].try(:length) || 0)
+      end
+      seriesList.push({name: 'Year: ' + year.to_s, data: series})
+    end
+    # return seriesList.to_json
+    return {labels: labels, data: seriesList}.to_json
+  end
+
+  def dashboard_generic_prospect_total(results)
+    seriesList = Project::STATUS.collect { |obj| {name: obj, y: 0.0} }
+    results['status'].keys.each do |year|
+      Project::STATUS.each do |obj|
+        if series = seriesList.find {|data| data[:name] == obj}
+          series[:y] += results['status'][year][obj].try(:length) || 0
+        end
+      end
+    end
+    seriesList.delete_if { |h| h[:y] == 0 }
+    total_projects = seriesList.inject(0.0){|sum, elm| sum + elm[:y]}
+    seriesList = seriesList.each { |elm| elm[:y] = (elm[:y]/total_projects).round(2) }.sort_by {|elm| elm[:y]}
+    seriesList.last[:sliced] = true
+    seriesList.last[:selected] = true
+    return seriesList.to_json
+  end
+
+  def dashboard_industry_report(data)
+    grouped_result = data.values.collect { |year| year.values }.flatten
+                         .select { |p| p if p.business_type == "New Business" }
+                         .group_by {|p| p.industry_type_id}
+    seriesList = grouped_result.collect { |obj| {name: '', y: 0.0} }
+    total_projects = grouped_result.inject(0.0) { |result, obj| result + obj[1].count }
+    grouped_result.keys.each_with_index do |industry_id, indx|
+      seriesList[indx][:name] = current_org.industry_types.find(industry_id).name
+      each_value = grouped_result[industry_id].count
+      seriesList[indx][:y] = (each_value/total_projects).round(2)
+    end
+    seriesList.sort_by {|elm| elm[:y]}
+    seriesList.last[:sliced] = true
+    seriesList.last[:selected] = true
+    return seriesList.to_json
+  end
+
+end
--- app/models/project_type.rb
+++ app/models/project_type.rb
@@ -3,6 +3,8 @@ class ProjectType < ApplicationRecord
 
   PROJECT_TYPES = ["Advanced Manfacturing", "Agriculture", "Big Box Retail", "Bio Medical", "Call Center", "Data Center", "Industrial", "Information Technology", "Manufacturing", "Restaurant", "Retail", "Warehouse"]
 
+  scope :filter_by_id, ->(ids) { where('id IN (?)', ids) }
+
   def self.create_configs( org_id )
     PROJECT_TYPES.each do |name|
       self.create!( name: name, status: "Active", organization_id: org_id )

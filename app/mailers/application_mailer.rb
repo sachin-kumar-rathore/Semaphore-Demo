a43685a03c568dd8fe40563e030d33688b1c2883
Sachin <skrathore.ror@gmail.com>
2018-03-26 17:21:38 +0530

    Change from-email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: '"ED Leader Tracker" <no-reply@notifications.edleadtracker.com>'
   layout 'mailer'
 end
